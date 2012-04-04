41c96e0a8be212f5478e7801ac8f5f66e1186fe0
Andy Sayler <andy.sayler@gmail.com>
2012-04-03 21:16:26 -0600

    Fixed compile warnings in original code

--- simulator.c
+++ simulator.c
@@ -1,8 +1,10 @@
 #include <stdio.h> 
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h> 
 #include <stdarg.h> 
-#include <signal.h> 
+#include <signal.h>
+#include <time.h> 
 
 #include "simulator.h"
 
@@ -82,7 +84,7 @@ static void sim_log(long type, const char *format, ...) {
     va_list ap; 
     if (log_port&type) { 
 	va_start(ap, format);
-	fprintf(stderr,"%08d: ",sysclock); vfprintf(stderr,format,ap); 
+	fprintf(stderr,"%08ld: ",sysclock); vfprintf(stderr,format,ap); 
 	va_end(ap);
     } 
 } 
@@ -140,22 +142,21 @@ typedef struct process {
 
 static void process_print(Process *q) { 
    long i; 
-   fprintf(stderr,"pc=%d",q->pc); 
-   fprintf(stderr," nbcontexts=%d",q->nbcontexts); 
-   fprintf(stderr," active=%d",q->active); 
-   fprintf(stderr," compute=%d",q->compute); 
-   fprintf(stderr," block=%d",q->block); 
-   fprintf(stderr," npages=%d",q->npages); 
-   fprintf(stderr," pid=%d",q->pid); 
-   fprintf(stderr," kind=%d",q->kind); 
+   fprintf(stderr,"pc=%ld",q->pc); 
+   fprintf(stderr," nbcontexts=%ld",q->nbcontexts); 
+   fprintf(stderr," active=%ld",q->active); 
+   fprintf(stderr," compute=%ld",q->compute); 
+   fprintf(stderr," block=%ld",q->block); 
+   fprintf(stderr," npages=%ld",q->npages); 
+   fprintf(stderr," pid=%ld",q->pid); 
+   fprintf(stderr," kind=%ld",q->kind); 
    fprintf(stderr," { "); 
    for (i=0; i<MAXPROCPAGES; i++) { 
-	fprintf(stderr,"%d ",q->pages[i]); 
+	fprintf(stderr,"%ld ",q->pages[i]); 
    } 
    fprintf(stderr,"}\n"); 
 } 
 
-// static Process processes[MAXPROCESSES]; /* since global, it's 0'd */ 
 static Process *processes[MAXPROCESSES]; 
 
 #include "programs.c" 
@@ -231,7 +232,7 @@ static void bcontext_init( Bcontext *c, Branch *b) {
             c->brings[c->bsize++]=1; // one failure to escape loop
         } 
     } else { 
-	fprintf(stderr, "unknown btype %d\n", (long)(b->btype)); 
+	fprintf(stderr, "unknown btype %ld\n", (long)(b->btype)); 
     } 
 } 
 
@@ -250,7 +251,7 @@ static long bcontext_decide(Bcontext *c) {
 } 
 
 static void process_clear(Process *q) { 
-   long i,j; 
+   long i; 
    q->pc = 0; 
    q->compute=q->block=0; 
    q->program = NULL; 
@@ -271,7 +272,7 @@ static void process_clear(Process *q) {
 
 /* load a program into a process */ 
 static void process_load(Process *q, Program *p, int pid, int kind) { 
-   long i,j; 
+   long i; 
    q->pc = 0; 
    q->compute=q->block=0; 
    q->program = p; 
@@ -307,11 +308,11 @@ static void process_unload(int pnum, Process *q) {
 static void process_dobranch(int pnum, Process *q, Branch *b, Bcontext *c) {
    if (bcontext_decide(c)) { 
 	// must document where we branched from
-       if (output) fprintf(output, "%d,%d,%d,%d,%d,branch_from\n", 
+       if (output) fprintf(output, "%ld,%d,%ld,%ld,%ld,branch_from\n", 
 		sysclock, pnum, q->pid, q->kind, q->pc); 
        q->pc = b->whereto; 
 	// and where we branched to
-       if (output) fprintf(output, "%d,%d,%d,%d,%d,branch_to\n", 
+       if (output) fprintf(output, "%ld,%d,%ld,%ld,%ld,branch_to\n", 
 		sysclock, pnum, q->pid, q->kind, q->pc); 
        sim_log(LOG_BRANCH,"process %2d; pc %04d: branch\n",pnum, q->pc); 
    } else { 
@@ -338,7 +339,7 @@ static long process_step(int pnum, Process *q) {
    if (q->pages[page]!=0) { 
 	if (!q->blocked[page]) { 
 	    sim_log(LOG_BLOCK,"process=%2d page=%3d blocked\n",pnum,page);
-	    if (output) fprintf(output, "%d,%d,%d,%d,%d,blocked\n", 
+	    if (output) fprintf(output, "%ld,%d,%ld,%ld,%ld,blocked\n", 
 		sysclock, pnum, q->pid, q->kind, q->pc); 
 	    q->blocked[page]=TRUE; 
 	}
@@ -346,7 +347,7 @@ static long process_step(int pnum, Process *q) {
    } else { 
 	if (q->blocked[page]) { 
 	    sim_log(LOG_BLOCK,"process=%2d page=%3d unblocked\n",pnum,page);
-	    if (output) fprintf(output, "%d,%d,%d,%d,%d,unblocked\n",
+	    if (output) fprintf(output, "%ld,%d,%ld,%ld,%ld,unblocked\n",
 		sysclock,pnum, q->pid, q->kind, q->pc);
 	    q->blocked[page]=FALSE; 
         } 
@@ -359,7 +360,7 @@ static long process_step(int pnum, Process *q) {
    while (min+1<max) { 
        long mid=(min+max)/2; 
        if (pc==q->program->exits[mid]) { 
-	    if (output) fprintf(output, "%d,%d,%d,%d,%d,exit\n", 
+	    if (output) fprintf(output, "%ld,%d,%ld,%ld,%ld,exit\n", 
 		sysclock, pnum, q->pid, q->kind, q->pc);
 	    return FALSE; 
        } 
@@ -367,7 +368,7 @@ static long process_step(int pnum, Process *q) {
        else                                 min=mid; 
    } 
    if (pc==q->program->exits[min] || pc==q->program->exits[max]) { 
-	if (output) fprintf(output, "%d,%d,%d,%d,%d,exit\n", 
+	if (output) fprintf(output, "%ld,%d,%ld,%ld,%ld,exit\n", 
 	    sysclock, pnum, q->pid, q->kind, q->pc);
 	return FALSE; 
    } 
@@ -388,10 +389,10 @@ static long process_step(int pnum, Process *q) {
    if (pc==b[max].wherefrom) { process_dobranch(pnum,q,b+max,c+max); return TRUE; } 
    q->pc++; /* default action */ 
    if (q->pc<0 || q->pc>q->program->size) { 
-	if (output) fprintf(output, "%d,%d,%d,%d,%d,out_of_range\n", 
+	if (output) fprintf(output, "%ld,%d,%ld,%ld,%ld,out_of_range\n", 
 	    sysclock, pnum, q->pid, q->kind, q->pc);
 	q->pc=0; /* start over */ 
-	if (output) fprintf(output, "%d,%d,%d,%d,%d,restart\n", 
+	if (output) fprintf(output, "%ld,%d,%ld,%ld,%ld,restart\n", 
 	    sysclock, pnum, q->pid, q->kind, q->pc);
    } 
    return TRUE; 
@@ -410,7 +411,7 @@ int pageout(int process, int page) {
     if (processes[process]->pages[page]>0) 
 	return FALSE; /* not available to swap out */ 
 sim_log(LOG_PAGE,"process=%2d page=%3d start pageout\n",process,page);
-    if (pages) fprintf(pages,"%d,%d,%d,%d,%d,going\n",
+    if (pages) fprintf(pages,"%ld,%d,%d,%ld,%ld,going\n",
 	sysclock,process,page,processes[process]->pid, processes[process]->kind); 
     processes[process]->pages[page]=-1; return TRUE;
 } 
@@ -429,7 +430,7 @@ int pagein(int process, int page) {
     if (processes[process]->pages[page]!=-PAGEWAIT ) 
 	return FALSE; /* not yet out */ 
     sim_log(LOG_PAGE,"process=%2d page=%3d start pagein\n",process,page);
-    if (pages) fprintf(pages,"%d,%d,%d,%d,%d,coming\n",
+    if (pages) fprintf(pages,"%ld,%d,%d,%ld,%ld,coming\n",
 	sysclock,process,page,processes[process]->pid, processes[process]->kind); 
     processes[process]->pages[page]=PAGEWAIT; pagesavail--; return TRUE; 
 } 
@@ -443,7 +444,7 @@ static long queuetype[QUEUESIZE];
 static Process queue[QUEUESIZE];
 static long queueend; 
 static void initqueue() { 
-   long i,j,repeats; 
+   long i,repeats; 
    for (i=0; i<QUEUESIZE; i++) queuetype[i]=i%PROGRAMS; 
    // for (i=0; i<QUEUESIZE; i++) queuetype[i]=lrand48()%PROGRAMS; 
    for (repeats=0; repeats<10; repeats++) 
@@ -483,7 +484,7 @@ static void allprint() {
     for (i=0; i<MAXPROCESSES/2; i++) { 
 	if (i) fprintf(stderr," | "); 
 	if (processes[i] && processes[i]->active) { 
-	    fprintf(stderr,"%04d",processes[i]->pc); 
+	    fprintf(stderr,"%04ld",processes[i]->pc); 
         } else { 
 	    fprintf(stderr,"----"); 
         }
@@ -497,23 +498,23 @@ static void allprint() {
 		int pcblock =  processes[i]->pc/PAGESIZE; 
 		if (j==pcblock) { 
 		    if (processes[i]->pages[j]>0) 
-			fprintf(stderr,"*i%3d",processes[i]->pages[j]); 
+			fprintf(stderr,"*i%3ld",processes[i]->pages[j]); 
 		    else if (processes[i]->pages[j]==0) 
-			fprintf(stderr,"*=in ",processes[i]->pages[j]); 
+			fprintf(stderr,"*=in "); 
 		    else if (processes[i]->pages[j]==-100) 
-			fprintf(stderr,"*=out",processes[i]->pages[j]); 
+			fprintf(stderr,"*=out"); 
 		    else 
-			fprintf(stderr,"*o%3d",100+processes[i]->pages[j]); 
+			fprintf(stderr,"*o%3ld",100+processes[i]->pages[j]); 
 		    // fprintf(stderr,"*%4d",processes[i]->pages[j]); 
 	  	} else { 
 		    if (processes[i]->pages[j]>0) 
-			fprintf(stderr," i%3d",processes[i]->pages[j]); 
+			fprintf(stderr," i%3ld",processes[i]->pages[j]); 
 		    else if (processes[i]->pages[j]==0) 
-			fprintf(stderr," =in ",processes[i]->pages[j]); 
+			fprintf(stderr," =in "); 
 		    else if (processes[i]->pages[j]==-100) 
-			fprintf(stderr," =out",processes[i]->pages[j]); 
+			fprintf(stderr," =out"); 
 		    else 
-			fprintf(stderr," o%3d",100+processes[i]->pages[j]); 
+			fprintf(stderr," o%3ld",100+processes[i]->pages[j]); 
 		    // fprintf(stderr," %4d",processes[i]->pages[j]); 
 		} 
 	    } else { 
@@ -537,7 +538,7 @@ static void allprint() {
     for (i=MAXPROCESSES/2; i<MAXPROCESSES; i++) {
 	if (i-MAXPROCESSES/2) fprintf(stderr," | "); 
 	if (processes[i] && processes[i]->active) { 
-	    fprintf(stderr,"%04d",processes[i]->pc); 
+	    fprintf(stderr,"%04ld",processes[i]->pc); 
         } else { 
 	    fprintf(stderr,"----"); 
         }
@@ -551,23 +552,23 @@ static void allprint() {
 		int pcblock =  processes[i]->pc/PAGESIZE; 
 		if (j==pcblock) { 
 		    if (processes[i]->pages[j]>0) 
-			fprintf(stderr,"*i%3d",processes[i]->pages[j]); 
+			fprintf(stderr,"*i%3ld",processes[i]->pages[j]); 
 		    else if (processes[i]->pages[j]==0) 
-			fprintf(stderr,"*=in ",processes[i]->pages[j]); 
+			fprintf(stderr,"*=in "); 
 		    else if (processes[i]->pages[j]==-100) 
-			fprintf(stderr,"*=out",processes[i]->pages[j]); 
+			fprintf(stderr,"*=out"); 
 		    else 
-			fprintf(stderr,"*o%3d",100+processes[i]->pages[j]); 
+			fprintf(stderr,"*o%3ld",100+processes[i]->pages[j]); 
 		    // fprintf(stderr,"*%4d",processes[i]->pages[j]); 
 	  	} else {
 		    if (processes[i]->pages[j]>0) 
-			fprintf(stderr," i%3d",processes[i]->pages[j]); 
+			fprintf(stderr," i%3ld",processes[i]->pages[j]); 
 		    else if (processes[i]->pages[j]==0) 
-			fprintf(stderr," =in ",processes[i]->pages[j]); 
+			fprintf(stderr," =in "); 
 		    else if (processes[i]->pages[j]==-100) 
-			fprintf(stderr," =out",processes[i]->pages[j]); 
+			fprintf(stderr," =out"); 
 		    else 
-			fprintf(stderr," o%3d",100+processes[i]->pages[j]); 
+			fprintf(stderr," o%3ld",100+processes[i]->pages[j]); 
 		    // fprintf(stderr," %4d",processes[i]->pages[j]); 
 		} 
 	    } else { 
@@ -591,20 +592,20 @@ static void allinit () {
 	    processes[i]=dequeue(); 
 
 	    sim_log(LOG_LOAD,"process %2d; pc %04d: loaded\n",i, processes[i]->pc); 
-	    if (output) fprintf(output, "%d,%d,%d,%d,%d,load\n", 
+	    if (output) fprintf(output, "%ld,%ld,%ld,%ld,%ld,load\n", 
 		sysclock, i, processes[i]->pid, 
 		processes[i]->kind, processes[i]->pc);
 	    if (pages) { 
 		long j;
 		for (j=0; j<MAXPROCPAGES; j++) 
-		    fprintf(pages,"%d,%d,%d,%d,%d,out\n",
+		    fprintf(pages,"%ld,%ld,%ld,%ld,%ld,out\n",
 			sysclock,i,j,processes[i]->pid,processes[i]->kind); 
 	    } 
 	} 
     } 
 } 
 
-static int allscore() { 
+static void allscore() { 
     int i; 
     int block=0; 
     int compute=0; 
@@ -625,13 +626,13 @@ static void allstep () {
 	if (!process_step(i,processes[i])) { 
 	    if (processes[i] && processes[i]->active) { 
 		// document final PC position 
-		if (output) fprintf(output, "%d,%d,%d,%d,%d,unload\n", 
+		if (output) fprintf(output, "%ld,%ld,%ld,%ld,%ld,unload\n", 
 		    sysclock, i, processes[i]->pid, 
 		    processes[i]->kind, processes[i]->pc);
 		if (pages) { 
 		    long j;
 		    for (j=0; j<MAXPROCPAGES; j++) 
-			fprintf(pages,"%d,%d,%d,%d,%d,out\n",
+			fprintf(pages,"%ld,%ld,%ld,%ld,%ld,out\n",
 			    sysclock,i,j,processes[i]->pid, processes[i]->kind); 
 		} 
 		process_unload(i,processes[i]); 
@@ -640,7 +641,7 @@ static void allstep () {
             if (!empty()) {
 		processes[i]=dequeue();
 	        sim_log(LOG_LOAD,"process %2d; pc %04d: loaded\n",i, processes[i]->pc); 
-		if (output) fprintf(output, "%d,%d,%d,%d,%d,load\n", 
+		if (output) fprintf(output, "%ld,%ld,%ld,%ld,%ld,load\n", 
 		    sysclock, i, processes[i]->pid, 
 		    processes[i]->kind, processes[i]->pc);
 	    } 
@@ -684,7 +685,7 @@ static int allblocked() {
     } 
 } 
 
-static long allage () { 
+static void allage () { 
    long i; 
    for (i=0; i<procs; i++) { 
        if (processes[i] && processes[i]->active) { 
@@ -696,7 +697,7 @@ static long allage () {
 		    processes[i]->pages[j]--; 
 		    if (processes[i]->pages[j]==0) { 
 			sim_log(LOG_PAGE,"process=%2d page=%3d end   pagein\n",i,j);
-			if (pages) fprintf(pages,"%d,%d,%d,%d,%d,in\n",
+			if (pages) fprintf(pages,"%ld,%ld,%ld,%ld,%ld,in\n",
 			    sysclock,i,j,processes[i]->pid, processes[i]->kind); 
 		    } 
 		} else if (processes[i]->pages[j]<0 
@@ -704,7 +705,7 @@ static long allage () {
 		    processes[i]->pages[j]--; 
 		    if(processes[i]->pages[j]==-PAGEWAIT) { 
 			sim_log(LOG_PAGE,"process=%2d page=%3d end   pageout\n",i,j);
-			if (pages) fprintf(pages,"%d,%d,%d,%d,%d,out\n",
+			if (pages) fprintf(pages,"%ld,%ld,%ld,%ld,%ld,out\n",
 			    sysclock,i,j,processes[i]->pid, processes[i]->kind); 
 			pagesavail++; 
 		    } 
@@ -736,12 +737,12 @@ static void callyou() {
     pageit(pentry); 	/* call your routine */ 
 } 
 
-int main(long argc, char **argv, char **envp) { 
-    // fprintf(stderr,"APOLOGIES! The simulator isn't working...!\n"); 
-    // fprintf(stderr, "see news!\n"); 
-    // exit(1); 
-    signal(SIGINT, endit); 
+int main(int argc, char **argv) { 
+    
     long i,errors=0,help=0; 
+ 
+    signal(SIGINT, endit); 
+    
     log_port=LOG_ALWAYS; 
     for (i=1; i<argc; i++) { 
 	if (strcmp(argv[i],"-help")==0) { 
@@ -759,7 +760,7 @@ int main(long argc, char **argv, char **envp) {
 	} else if (strcmp(argv[i],"-dead")==0) { 
 	    log_port |= LOG_DEAD; 
 	} else if (strcmp(argv[i],"-seed")==0) { 
-	    if (sscanf(argv[++i],"%d",&seed)!=1) {
+	    if (sscanf(argv[++i],"%ld",&seed)!=1) {
 		fprintf(stderr,
 			"%s: could not read random seed from command line\n",
 			argv[0]); 
@@ -786,7 +787,7 @@ int main(long argc, char **argv, char **envp) {
 		errors++; 
 	    } 
 	} else if (strcmp(argv[i],"-procs")==0) { 
-	    if (sscanf(argv[++i],"%d",&procs)!=1) {
+	    if (sscanf(argv[++i],"%ld",&procs)!=1) {
 		fprintf(stderr,
 			"%s: could not read number of processors from command line\n",
 			argv[0]); 
@@ -812,7 +813,13 @@ int main(long argc, char **argv, char **envp) {
 	fprintf(stderr, "  -seed 512  set random seed to 512\n"); 
 	fprintf(stderr, "  -procs 4   run only four processors\n"); 
 	fprintf(stderr, "  -dead      detect deadlocks\n"); 
-	fprintf(stderr, "  -csv       generate output.csv and pages.csv for graphing\n"); 
+	fprintf(stderr, "  -csv       generate output.csv and pages.csv for graphing\n");
+	if(errors) {
+	    return EXIT_FAILURE;
+	}
+	else {
+	    return EXIT_SUCCESS;
+	}
     } 
     if (seed==0) { 
 	seed = (time(NULL)*38491+71831+time(NULL)*time(NULL))&((1<<30)-1); 
@@ -830,5 +837,7 @@ int main(long argc, char **argv, char **envp) {
 	allblocked();    // deadlock detection 
     } 
     allscore(); 
-    // printf("pagesavail=%d\n",pagesavail);
+
+    return EXIT_SUCCESS;
+
 } 
